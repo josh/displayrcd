72377160170dff42315a8b8ef96d45bd5dfd1dd7
Joshua Peek <josh@joshpeek.com>
2020-12-27 14:34:09 -0800

    Upgrade argument parser

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.1"),
     ],
     targets: [
         .target(
