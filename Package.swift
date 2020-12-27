4d5599848850a0e4d751a638d32cf15ab6c74b2c
Joshua Peek <josh@joshpeek.com>
2020-12-27 14:31:46 -0800

    Require latest Swift and OS

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "DisplayMode",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v11),
     ],
     products: [
         .executable(
